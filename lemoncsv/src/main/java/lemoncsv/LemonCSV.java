d3bada9306f3f03b6dc45bf4481c390f3709d7f0
mike fidel <mikefidel.us@gmail.com>
2019-10-14 00:09:01 -0700

    Can execute jar app files.

--- lemoncsv/src/main/java/lemoncsv/LemonCSV.java
+++ lemoncsv/src/main/java/lemoncsv/LemonCSV.java
@@ -1,4 +1,18 @@
 package lemoncsv;
 
+import lemonade.configuration.Configuration;
+
 public class LemonCSV {
+    Configuration cfg;
+
+    public static void main(String[] args) {
+
+        LemonCSV app = new LemonCSV();
+        app.start();
+    }
+
+    public void start() {
+        System.err.println("LemonCSV started");
+    }
+
 }
--- lemonpoi/src/main/java/lemonpoi/LemonPOI.java
+++ lemonpoi/src/main/java/lemonpoi/LemonPOI.java
@@ -1,4 +1,17 @@
 package lemonpoi;
 
+import lemonade.configuration.Configuration;
+
 public class LemonPOI {
+    Configuration cfg;
+
+    public static void main(String[] args) {
+        LemonPOI app = new LemonPOI();
+        app.start();
+    }
+
+    public void start() {
+        System.err.println("LemonPOI started");
+    }
+
 }
